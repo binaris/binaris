e04ac4fb94320dfb7c5dbeff6a85e717e3cc18fb
ryland goldstein <rylandgoldstein@gmail.com>
2017-09-14 10:17:44 -0700

    small clean ups in index.js
    removed a lot of unnecessary printouts and
    small kruft

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 // here we just grab all our SDK functions that we plan to use
-const { init, deploy, invoke, destroy, help,
-  info, login, logout, signup } = require('./sdk/sdk');
+// invoke, destroy, help, info, login, logout, signup
+const { init, deploy } = require('./sdk/sdk');
 
 // create our basic logger
 const logger = require('./sdk/shared/loggerInit.js');
@@ -44,11 +44,55 @@ const validateFunctionName = async function validateFunctionName(name) {
   return response;
 };
 
+const initHandler = async function initHandler(options) {
+  logger.binaris.info('Initializing Binaris function...'.yellow);
+  const initPayload = {
+    functionName: undefined,
+    functionPath: undefined,
+  };
+  if (options.functionName) {
+    const answer = await validateFunctionName(options.functionName);
+    if (answer.valid) {
+      initPayload.functionName = options.functionName;
+    } else {
+      logger.binaris.error(answer.error.red);
+      process.exit(0);
+    }
+  } else {
+    while (!initPayload.functionName) {
+      const potentialName = moniker.choose();
+      const answer = await validateFunctionName(potentialName);
+      if (answer.valid) {
+        initPayload.functionName = potentialName;
+      }
+    }
+  }
+  if (options.path) {
+    if (fs.existsSync(options.path)) {
+      if (fs.lstatSync(options.path).isDirectory()) {
+        initPayload.functionPath = path.resolve(options.path);
+      } else {
+        logger.binaris.error(`path: ${options.path} is not a directory!`);
+        process.exit(0);
+      }
+    } else {
+      logger.binaris.error(`path: ${options.path} is invalid!`.red);
+      process.exit(0);
+    }
+  } else {
+    initPayload.functionPath = process.cwd();
+  }
 
-// ignore this, its just temporary to simulate a more 'real' experience
-const defaultSleep = 500;
-const asyncTimeout = function timeout(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  // now we actually call our initialize function and then immediately
+  // determine if was successfully completed
+  const initCompletion = await init(initPayload);
+  if (initCompletion.success) {
+    logger.binaris.info(`sucessfully initialized function ${initPayload.functionName}`.green);
+    logger.binaris.info('function details:'.yellow);
+    logger.binaris.info(JSON.stringify(initPayload, null, 2).yellow);
+  } else {
+    logger.binaris.error(initCompletion.error.red);
+  }
 };
 
 commander
@@ -57,115 +101,55 @@ commander
 
 commander
   .command('init')
-  .description('creates the skeleton of a binaris function')
+  .description('creates the skeleton of a Binaris function')
   .option('-f, --functionName [functionName]', 'The name of the function you are creating')
   .option('-p, --path [path]', 'The path to create your function(default is pwd)')
-  .action(async (options) => {
-    logger.binaris.info('beginning Binaris function initialization...'.yellow);
-    const initPayload = {
-      functionName: undefined,
-      functionPath: undefined,
-    };
-    logger.binaris.info('validating function name...'.yellow);
-    // remove me
-    await asyncTimeout(defaultSleep);
-    if (options.functionName) {
-      const answer = await validateFunctionName(options.functionName);
-      if (answer.valid) {
-        initPayload.functionName = options.functionName;
-      } else {
-        logger.binaris.error(answer.error.red);
-        process.exit(0);
-      }
-    } else {
-      while (!initPayload.functionName) {
-        const potentialName = moniker.choose();
-        const answer = await validateFunctionName(potentialName);
-        if (answer.valid) {
-          initPayload.functionName = potentialName;
-        }
-      }
-    }
-    logger.binaris.info('function name is valid!'.green);
-    logger.binaris.info('validating function path...'.yellow);
-    // remove me
-    await asyncTimeout(defaultSleep);
-    if (options.path) {
-      if (fs.existsSync(options.path)) {
-        if (fs.lstatSync(options.path).isDirectory()) {
-          initPayload.functionPath = path.resolve(options.path);
-        } else {
-          logger.binaris.error(`path: ${options.path} is not a directory!`);
-          process.exit(0);
-        }
-      } else {
-        logger.binaris.error(`path: ${options.path} is invalid!`.red);
-        process.exit(0);
-      }
-    } else {
-      initPayload.functionPath = process.cwd();
-    }
-    logger.binaris.info('function path is valid!'.green);
-
-    // now we actually call our initialize function and then immediately
-    // determine if was successfully completed
-    logger.binaris.info('attempting to initialize...'.yellow);
-    // remove me
-    await asyncTimeout(defaultSleep);
-    const initCompletion = await init(initPayload);
-    if (initCompletion.success) {
-      logger.binaris.info(`sucessfully initialized function ${initPayload.functionName}`.green);
-      logger.binaris.info('function details:'.yellow);
-      logger.binaris.info(JSON.stringify(initPayload, null, 2).yellow);
-    } else {
-      logger.binaris.error(initCompletion.error.red);
-    }
-  });
+  .action(initHandler);
 
 commander
   .command('deploy')
-    .description('')
-      .action();
+  .description('')
+  .action();
 
 commander
   .command('invoke')
-    .description('')
-      .action();
+  .description('')
+  .action();
 
 commander
   .command('destroy')
-    .description('')
-      .action(() => { noSupport('destroy'); });
+  .description('')
+  .action(() => { noSupport('destroy'); });
 
 commander
   .command('help')
-    .description('')
-      .action(() => { noSupport('help'); });
+  .description('')
+  .action(() => { noSupport('help'); });
 
 commander
   .command('info')
-    .description('')
-      .action(() => { noSupport('info'); });
+  .description('')
+  .action(() => { noSupport('info'); });
 
 commander
   .command('login')
-    .description('')
-      .action(() => { noSupport('login'); });
+  .description('')
+  .action(() => { noSupport('login'); });
 
 commander
   .command('logout')
-    .description('')
-      .action(() => { noSupport('logout'); });
+  .description('')
+  .action(() => { noSupport('logout'); });
 
 commander
   .command('signup')
-    .description('')
-      .action(() => { noSupport('signup'); });
+  .description('')
+  .action(() => { noSupport('signup'); });
 
 commander
   .command('feedback')
-    .description('')
-      .action(() => { noSupport('feedback'); });
+  .description('')
+  .action(() => { noSupport('feedback'); });
 
 commander
   .parse(process.argv);
