b62e5d5dcdec41920db301d9af7452220981a956
Michael Adda <michael@binaris.com>
2017-09-17 13:28:20 +0300

    rename event to input in signature to reflect its meaning

--- sdk/init/functionTemplates/nodejs/function.js
+++ sdk/init/functionTemplates/nodejs/function.js
@@ -1,9 +1,9 @@
-module.exports.handler = function (event, context, callback) {
+module.exports.handler = function (input, context, callback) {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Welcome to Binaris',
-      input: event,
+      input,
       context,
     }),
   };
