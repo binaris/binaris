5c0745738837f009641a75ad5930dc7af8b4bd73
Michael Adda <michael@binaris.com>
2017-09-21 10:00:22 +0300

    further clean up

--- sdk/init/init.js
+++ sdk/init/init.js
@@ -7,35 +7,38 @@ const util = require('../shared/util');
 const templateDir = './functionTemplates/nodejs/';
 
 const init = async function init(functionName, functionPath) {
-  if (functionName && functionPath) {
-    log.debug('attempting to load template files for function dir creation');
-    // parse our templated yml and make the necessary modifications
-    const templatePath = path.join(__dirname, templateDir);
-    const binarisConf = util.loadBinarisConf(templatePath);
-    const templateName = util.getFuncName(binarisConf);
-    const funcConf = util.getFuncConf(binarisConf, templateName);
-    // replace the generic function name with the actual name
-    util.addFuncConf(binarisConf, functionName, funcConf);
-    util.delFuncConf(binarisConf, templateName);
-
-    const newDir = path.join(functionPath, functionName);
-    // ensure that the function directory doesn't already exist
-    try {
-      fs.mkdirSync(newDir);
-    } catch (err) {
-      log.debug(err);
-      throw new Error(`Function w/ name ${functionName} could not be initialized because a directory already exists with that name!`);
-    }
-    log.debug('loading and replicating all template files');
-
-    // now we have to write out all our files that we've modified
-    const file = funcConf.file;
-    fs.writeFileSync(path.join(newDir, file),
-      fs.readFileSync(path.join(__dirname, templateDir, file)));
-    util.saveBinarisConf(newDir, binarisConf);
-    return;
+  if (!functionName) {
+    throw new Error('Invalid function name provided!');
   }
-  throw new Error('Invalid function path or function name provided!');
+  if (!functionPath) {
+    throw new Error('Invalid function path provided!');
+  }
+
+  log.debug('attempting to load template files for function dir creation');
+  // parse our templated yml and make the necessary modifications
+  const templatePath = path.join(__dirname, templateDir);
+  const binarisConf = util.loadBinarisConf(templatePath);
+  const templateName = util.getFuncName(binarisConf);
+  const funcConf = util.getFuncConf(binarisConf, templateName);
+  // replace the generic function name with the actual name
+  util.addFuncConf(binarisConf, functionName, funcConf);
+  util.delFuncConf(binarisConf, templateName);
+
+  const newDir = path.join(functionPath, functionName);
+  // ensure that the function directory doesn't already exist
+  try {
+    fs.mkdirSync(newDir);
+  } catch (err) {
+    log.debug(err);
+    throw new Error(`Function ${functionName} could not be initialized because a directory already exists with that name!`);
+  }
+  log.debug('loading and replicating all template files');
+
+  // now we have to write out all our files that we've modified
+  const file = funcConf.file;
+  fs.writeFileSync(path.join(newDir, file),
+    fs.readFileSync(path.join(__dirname, templateDir, file)));
+  util.saveBinarisConf(newDir, binarisConf);
 };
 
 module.exports = init;
