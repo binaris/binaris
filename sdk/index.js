d8e1e14bfc617e303b3cfc77ccfd93d67facb51f
ryland goldstein <rylandgoldstein@gmail.com>
2017-09-27 14:04:07 -0700

    removed unavailable commands
    removed commands which are not yet available
    via the SDK

--- sdk/index.js
+++ sdk/index.js
@@ -1,20 +1,8 @@
 // this is just a convenience/wrapper for the individual commands
 const deploy = require('./deploy/deploy');
 const invoke = require('./invoke/invoke');
-const destroy = require('./destroy/destroy');
-const help = require('./help/help');
-const info = require('./info/info');
-const login = require('./login/login');
-const logout = require('./logout/logout');
-const signup = require('./signup/signup');
 
 module.exports = {
   deploy,
   invoke,
-  destroy,
-  help,
-  info,
-  login,
-  logout,
-  signup,
 };
