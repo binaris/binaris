308739fc85b36b5facc2d8a35dec90f499f3a12f
Michael Adda <michael@binaris.com>
2017-10-01 11:11:19 +0300

    use https with deploy endpoint

--- sdk/deploy.js
+++ sdk/deploy.js
@@ -5,7 +5,7 @@ const request = require('request');
 // TODO: ensure that this is configured in a better way, having a single
 // variable in the deploy file is inadequate
 const deployEndpoint =
-  process.env.BINARIS_DEPLOY_ENDPOINT || 'api-staging.binaris.io:11011';
+  process.env.BINARIS_DEPLOY_ENDPOINT || 'api-staging.binaris.io';
 
 const deployFunction = async function uploadFunction(tarPath, conf, deployURL) {
   const options = {
@@ -31,7 +31,7 @@ const deployFunction = async function uploadFunction(tarPath, conf, deployURL) {
 
 // TODO: thing that returns metadata
 const deploy = async function deploy(funcName, funcConf, tarPath) {
-  const endpoint = urljoin(`http://${deployEndpoint}/v1/function`, funcName);
+  const endpoint = urljoin(`https://${deployEndpoint}/v1/function`, funcName);
   const response = await deployFunction(tarPath, funcConf, endpoint);
   if (response.statusCode !== 200) {
     throw new Error('Function was not deployed successfully, check logs for more details');
