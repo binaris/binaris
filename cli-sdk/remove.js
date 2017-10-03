1357432048ed131f7475c278861dfc399ce125fe
Ariel Shaqed (Scolnicov) <ariel.shaqed@gmail.com>
2017-10-03 11:48:26 +0300

    Allow remove fron config file.
    Explicit functionName still takes precedence, naturally.

--- cli-sdk/remove.js
+++ cli-sdk/remove.js
@@ -1,7 +1,10 @@
 const { remove } = require('../sdk');
+const YMLUtil = require('./binarisYML');
 
-const removeCLI = async function removeCLI(funcName) {
-  return await remove(funcName);
+const removeCLI = async function removeCLI(funcName, funcPath) {
+  const binarisConf = YMLUtil.loadBinarisConf(funcPath);
+  const configuredFuncName = YMLUtil.getFuncName(binarisConf);
+  return remove(funcName || configuredFuncName);
 };
 
 module.exports = removeCLI;
