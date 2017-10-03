9abcb40049d6bfe624a528a6972acd82176e4d84
Ariel Shaqed (Scolnicov) <ariel.shaqed@gmail.com>
2017-10-03 13:11:28 +0300

    Support remove by binaris directory, not just --functionName.

--- cli-sdk/remove.js
+++ cli-sdk/remove.js
@@ -1,9 +1,16 @@
 const { remove } = require('../sdk');
+const log = require('./logger');
 const YMLUtil = require('./binarisYML');
 
 const removeCLI = async function removeCLI(funcName, funcPath) {
-  const binarisConf = YMLUtil.loadBinarisConf(funcPath);
-  const configuredFuncName = YMLUtil.getFuncName(binarisConf);
+  let configuredFuncName;
+  try {
+    const binarisConf = YMLUtil.loadBinarisConf(funcPath);
+    configuredFuncName = YMLUtil.getFuncName(binarisConf);
+  } catch (err) {
+    log.verbose('Failed to read config file', { err, functionName: funcName, path: funcPath });
+    // OK if functionName specified.
+  }
   return remove(funcName || configuredFuncName);
 };
 
