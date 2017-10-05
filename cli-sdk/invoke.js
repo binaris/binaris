851b8b72af571a63fc0fea6343b57fe0ff950595
Ryland Goldstein <rylandgoldstein@gmail.com>
2017-10-04 18:34:45 -0700

    enforce function name arg on invoke
    you are now required to provide the name of the function
    you wish to invoke.

--- cli-sdk/invoke.js
+++ cli-sdk/invoke.js
@@ -3,10 +3,13 @@ const { invoke } = require('../sdk');
 
 // invokes a binaris function that you have previously
 // deployed either through the CLI or other means
-const invokeCLI = async function invokeCLI(funcPath, funcData) {
+const invokeCLI = async function invokeCLI(funcPath, funcName, funcData) {
   const binarisConf = YMLUtil.loadBinarisConf(funcPath);
-  const funcName = YMLUtil.getFuncName(binarisConf);
-  return invoke(funcName, funcData);
+  const validFuncName = YMLUtil.getFuncName(binarisConf);
+  if (validFuncName !== funcName) {
+    throw new Error(`${funcName} is not a deployed function!`);
+  }
+  return invoke(validFuncName, funcData);
 };
 
 module.exports = invokeCLI;
