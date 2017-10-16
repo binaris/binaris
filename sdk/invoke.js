136b32a11868fbd54a0b2ad863de5be9e2d4f1a9
ryland goldstein <rylandgoldstein@gmail.com>
2017-10-16 15:34:35 -0700

    fixes based on review comments
    Addressed and fixed issues raised
    by PR comments

--- sdk/invoke.js
+++ sdk/invoke.js
@@ -1,14 +1,14 @@
 const urljoin = require('urljoin');
-const rp = require('request-promise');
+const rp = require('request-promise-native');
 const { invokeEndpoint } = require('./config');
 
 const invoke = async function invoke(funcName, funcData) {
   const endpoint = urljoin(`https://${invokeEndpoint}/v1/user/`, funcName);
   const options = {
     url: endpoint,
-    body: JSON.stringify(funcData),
-    headers: { 'Content-Type': 'application/json' },
+    body: funcData,
     resolveWithFullResponse: true,
+    json: true,
   };
   return rp.post(options);
 };
