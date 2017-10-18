27d949df0ca3a59800d53ad1f3417b90196d002e
Ryland Goldstein <rylandgoldstein@gmail.com>
2017-10-17 22:40:53 -0700

    error message improvement & code cleanup
    removed the unecessary redundant error message
    statements and instead moved logic to a wrapper
    function which all handlers are invoked via.

--- cli.js
+++ cli.js
@@ -11,11 +11,20 @@ const path = require('path');
 // 3rd party modules
 const commander = require('commander');
 
-const errorMessageAndExit = function errorMessageAndExit() {
-  if (!process.env.BINARIS_LOG_LEVEL) {
-    log.info('For more information set the BINARIS_LOG_LEVEL environment variable to debug, verbose, info, warn or error');
-  }
-  process.exit(1);
+const tryCatchWrapper = function tryCatchWrapper(funcToWrap) {
+  const wrapped = async function wrapped() {
+    try {
+      const args = [...arguments].splice(0);
+      await funcToWrap(...args);
+    } catch (err) {
+      log.error(err);
+      if (!process.env.BINARIS_LOG_LEVEL) {
+        log.info('For more information set the BINARIS_LOG_LEVEL environment variable to debug, verbose, info, warn or error');
+      }
+      process.exit(1);
+    }
+  };
+  return wrapped;
 };
 
 // attempts to parse a json and throws if an issue is encountered
@@ -42,46 +51,31 @@ const initHandler = async function initHandler(options) {
   // this is where the actual initialize function is called and immediately
   // evaluated to determine if was successfully completed
   const functionPath = getFuncPath(options);
-  try {
-    const finalName = await init(options.functionName, functionPath);
-    log.info(`Initialized function: ${finalName}`);
-    log.info(`To deploy: (cd ${functionPath}/${finalName}; bn deploy)`);
-  } catch (err) {
-    log.error(err.message);
-    errorMessageAndExit();
-  }
+  const finalName = await init(options.functionName, functionPath);
+  log.info(`Initialized function: ${finalName}`);
+  log.info(`To deploy: (cd ${functionPath}/${finalName}; bn deploy)`);
 };
 
 // simply handles the process of deploying a function and its
 // associated metadata to the Binaris cloud
 const deployHandler = async function deployHandler(options) {
   log.info('Deploying function...');
-  try {
-    const funcPath = getFuncPath(options);
-    const funcEndpoint = await deploy(funcPath);
-    log.info(`Function deployed. To invoke use:\n  curl ${funcEndpoint}\nor\n  bn invoke`);
-  } catch (err) {
-    log.error(err.message);
-    errorMessageAndExit();
-  }
+  const funcPath = getFuncPath(options);
+  const funcEndpoint = await deploy(funcPath);
+  log.info(`Function deployed. To invoke use:\n  curl ${funcEndpoint}\nor\n  bn invoke`);
 };
 
 // Removes a binaris function that you previously deployed.
 const removeHandler = async function removeHandler(options) {
-  try {
-    const { functionName } = options;
-    const funcPath = getFuncPath(options);
+  const { functionName } = options;
+  const funcPath = getFuncPath(options);
 
-    if (!functionName && !funcPath) {
-      throw new Error('Missing function name. Use --path or --functionName');
-    }
-    log.info(`Removing function: ${functionName || funcPath}...`);
-    await remove(functionName, funcPath);
-    log.info('Function removed');
-  } catch (err) {
-    log.error(err.message);
-    errorMessageAndExit();
+  if (!functionName && !funcPath) {
+    throw new Error('Missing function name. Use --path or --functionName');
   }
+  log.info(`Removing function: ${functionName || funcPath }...`);
+  await remove(functionName, funcPath);
+  log.info('Function removed');
 };
 
 // invokes a binaris function that has been previously
@@ -93,32 +87,27 @@ const invokeHandler = async function invokeHandler(functionName, options) {
   }
   const funcPath = getFuncPath(options);
   let funcData;
-  try {
-    let payloadJSON;
-    if (options.json) {
-      payloadJSON = options.json;
-    } else if (options.file) {
-      try {
-        payloadJSON = await fs.readFile(options.file, 'utf8');
-      } catch (err) {
-        throw new Error(`Invalid JSON file path: ${options.file}`);
-      }
+  let payloadJSON;
+  if (options.json) {
+    payloadJSON = options.json;
+  } else if (options.file) {
+    try {
+      payloadJSON = await fs.readFile(options.file, 'utf8');
+    } catch (err) {
+      throw new Error(`Invalid JSON file path: ${options.file}`);
     }
-
-    if (payloadJSON) {
-      funcData = attemptJSONParse(payloadJSON);
-      log.debug({ funcData });
-    }
-
-    const response = await invoke(funcPath, functionName, funcData);
-    if (response.statusCode !== 200) {
-      log.warn(`Function returned non standard status: ${response.statusCode}`);
-    }
-    log.info(response.body);
-  } catch (err) {
-    log.error(err.message);
-    errorMessageAndExit();
   }
+
+  if (payloadJSON) {
+    funcData = attemptJSONParse(payloadJSON);
+    log.debug({ funcData });
+  }
+
+  const response = await invoke(funcPath, functionName, funcData);
+  if (response.statusCode !== 200) {
+    log.warn(`Function returned non standard status: ${response.statusCode}`);
+  }
+  log.info(response.body);
 };
 
 commander
@@ -130,20 +119,20 @@ commander
   .description('Initialize a function from template')
   .option('-f, --functionName <functionName>', 'name for the generated fucntion (if omitted, a name will be chosen at random)')
   .option('-p, --path <path>', 'directory for the generated function (default is cwd)')
-  .action(initHandler);
+  .action(tryCatchWrapper(initHandler));
 
 commander
   .command('deploy')
   .description('Deploys a function to the cloud')
   .option('-p, --path <path>', 'path to function direcotry (where binaris.yml is)')
-  .action(deployHandler);
+  .action(tryCatchWrapper(deployHandler));
 
 commander
   .command('remove')
   .description('Remove a function from the cloud')
   .option('-f, --functionName <functionName>', 'name of function to remove')
   .option('-p, --path <path>', 'path to function')
-  .action(removeHandler);
+  .action(tryCatchWrapper(removeHandler));
 
 commander
   .command('invoke <functionName>')
@@ -151,7 +140,7 @@ commander
   .option('-p, --path <path>', 'path to function')
   .option('-j, --json <json>', 'JSON data to pass as event.body')
   .option('-f, --filePath <filePath>', 'path to a JSON file containing data to pass as event.body')
-  .action(invokeHandler);
+  .action(tryCatchWrapper(invokeHandler));
 
 commander
   .command('*', null, { noHelp: true })
