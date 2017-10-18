1dd62869b56d8f914c624b914c5b5a1f05ea2196
Ryland Goldstein <rylandgoldstein@gmail.com>
2017-10-18 11:19:30 -0700

    remove now requires a functionName
    remove now requires a functionName and takes no
    other arguments. this change was made because remove
    didn't actually need any other arguments since it wouldn't
    even verify a functionName if given a conf to cross reference.
    Furthermore remove now prints the name of the function
    it is removing.

--- cli-sdk/remove.js
+++ cli-sdk/remove.js
@@ -2,20 +2,8 @@ const { remove } = require('../sdk');
 const log = require('./logger');
 const YMLUtil = require('./binarisYML');
 
-const removeCLI = async function removeCLI(funcName, funcPath) {
-  if (funcName) {
-    return remove(funcName);
-  }
-
-  let configuredFuncName;
-  try {
-    const binarisConf = await YMLUtil.loadBinarisConf(funcPath);
-    configuredFuncName = YMLUtil.getFuncName(binarisConf);
-  } catch (err) {
-    log.verbose('Failed to read config file', { err, functionName: funcName, path: funcPath });
-    throw new Error(`Failed to read config file for ${funcPath}; specify function name to remove.`);
-  }
-  return remove(configuredFuncName);
+const removeCLI = async function removeCLI(funcName) {
+  return remove(funcName);
 };
 
 module.exports = removeCLI;
