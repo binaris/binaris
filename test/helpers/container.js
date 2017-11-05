6badd724ff7a98693bb86fb4cf76cbc665f31767
Ryland Goldstein <rylandgoldstein@gmail.com>
2017-11-04 19:24:03 -0700

    all of the tests running in separate dockers
    fully running and working tests each in their
    own docker containers(its kind of slow though)

---
+++ test/helpers/container.js
@@ -0,0 +1,101 @@
+const { execBash } = require('./spawnUtils');
+const uuidv4 = require('uuid/v4');
+
+class Container {
+  /**
+   * Creates a container handle with the specified Docker image name,
+   * sudo user password and mount directory. The mount directory is
+   * the location where the persistent volume should be mounted in the container.
+   *
+   * @param {string} imgName - the docker image name to use when creating this container
+   * @param {string} sudoPassword - the password for the sudo user of the image
+   * @param {string} mountDir - the dir to mount a volume on in the container
+   */
+  constructor(imgName, sudoPassword, mountDir) {
+    this.sudoCommand = `echo ${sudoPassword} | sudo -S `;
+    this.imgName = imgName;
+    this.mountDir = mountDir;
+  }
+
+  /**
+   * Creates a container with imgName and a defined volume.
+   * The container will last until removed as there is no
+   * Object lifetime management in JS.
+   *
+   * @param {string} flags - extra flags to pass to create
+   */
+  async create(flags) {
+    if (this.volumeName) {
+      throw new Error('Container has already been created!');
+    }
+    this.volumeName = `binaris${uuidv4().slice(0, 8)}`;
+    const create = await execBash(
+`docker create -v ${this.mountDir} --name ${this.volumeName} \
+${flags || ''} ${this.imgName} /bin/true`, false);
+    this.ID = create.slice(0, -1);
+    return this.ID;
+  }
+
+  /**
+   * Removes previously created container. Volume
+   * and all data will be lost in the process.
+   *
+   * @param {string} flags - extra flags to pass to remove
+   */
+  async remove(flags) {
+    if (!this.volumeName) {
+      throw new Error('Container has already been removed!');
+    }
+    const removed = await execBash(`docker rm ${flags || ''} ${this.ID}`, false);
+    this.volumeName = undefined;
+    this.ID = undefined;
+    return removed;
+  }
+
+  /**
+   * Gives the user and optional group access to the mounted
+   * volume of this container.
+   *
+   * @param {string} user - ubuntu user to give volume access to
+   * @param {string} group - ubuntu group to give volume access to
+   * @returns {string} - the output of the access modification attempt
+   */
+  async giveUserVolumeAccess(user, group) {
+    const chownVolume =
+`${this.sudoCommand}chown ${user}${group ? ':' + group : ''} ${this.mountDir} > /dev/null 2>&1`;
+    const chmodVolume =
+`${this.sudoCommand}chmod 700 ${this.mountDir} > /dev/null 2>&1`;
+    return this.run('', `${chownVolume} && ${chmodVolume}`, false);
+  }
+
+  /**
+   * Runs your docker container and attempts to invoke the provided
+   * command via bash. You will either receive and error object with
+   * the stdout, stderr and exit code or the stdout and an implied
+   * success.
+   *
+   * @param {string} flags -  extra flags to pass to run
+   * @param {string} runArgs - the run arguments to pass to bash
+   * @returns {object} - stdout of your run
+   */
+  async run(flags, runArgs, color = false) {
+    if (!this.volumeName) {
+      throw new Error('Please create container before trying to use run!');
+    }
+    return execBash(
+  `docker run --volumes-from ${this.volumeName} ${flags} ${this.imgName} bash -c \
+  "cd ${this.mountDir} && ${runArgs}"`, color);
+  }
+
+  /**
+   * Checks whether this container has beeen created.
+   *
+   * @returns - whether or not this container has been created
+   */
+  isCreated() {
+    return (this.volumeName !== undefined);
+  }
+}
+
+module.exports = Container;
+
--- test/test.js
+++ test/test.js
@@ -1,125 +1,103 @@
-// test runner
+// Test runner
 const { test } = require('ava');
 
-// external dependencies
-const proxyquire = require('proxyquire');
+// External dependencies
 const yaml = require('js-yaml');
 const fs = require('mz/fs');
 const globToRegExp = require('glob-to-regexp');
-const stringArgv = require('string-argv');
-const shell = require('shelljs');
-const clone = require('clone');
-// this is the base SDK used during the tests
-const binarisSDK = require('../sdk');
 
-// just the temporary way to control whether tests run locally
-const useMockSDK = process.env.BINARIS_MOCK_SDK > 0;
+// Create/run and remove a Docker container.
+const Container = require('./helpers/container');
 
-// the default logger is replaced in the tests so that
-// the tests can intercept and analyze the dialog. All output
-// directed towards this fake logger can be accessed through the
-// 'logOutput' variable or to dump the logs use the getOutputString method
-const createLogger = function createLogger() {
-  const logger = { logOutput: [] };
-  const defaultLogFunction = function defaultLog(text) {
-    logger.logOutput.push(text);
-  };
+// The name of our Docker image.
+const dockerImage = 'binaris';
 
-  logger.info = defaultLogFunction;
-  logger.warn = defaultLogFunction;
-  logger.error = function errorlog(text) {
-    logger.logOutput.push(`bn: ${text}`);
-  };
-  // simple convenience method which retrieves
-  // all text currently held by this logger(erases after retrieval)
-  logger.releaseOutputString = function releaseOutputString() {
-    const finalText =
-      logger.logOutput.reduce((aggr, logItem) =>
-       `${(aggr ? `${aggr}\n` : '')}${logItem}`);
-    logger.logOutput = [];
-    return finalText;
-  };
-  return logger;
-};
+// Our docker user/group information.
+const dockerUser = 'dockeruser';
+const dockerGroup = 'dockeruser';
+const dockerPassword = 'binaris';
 
-const createCLIStub = function createCLIStub(stubSDK, logger) {
-  const actionStub = function actionStub(action) {
-    return proxyquire(`../cli-sdk/${action}`, { '../sdk': stubSDK });
-  };
-  return proxyquire('../cli', {
-    './cli-sdk': proxyquire('../cli-sdk', {
-      './deploy': actionStub('deploy'),
-      './invoke': actionStub('invoke'),
-      './remove': actionStub('remove'),
-      './logger': logger,
-    }),
-  });
-};
+// Directory to mount the test volume on inside Docker.
+const testMountDir = '/home/dockeruser/test';
 
-const fakeSDK = function fakeSDK(changes) {
-  const empty = {
-    invoke: async () => {},
-    deploy: async (apiKey, funcName) => funcName,
-    remove: async () => {},
-  };
-  return Object.assign({}, empty, changes);
-};
+// This is needed to mess with the Docker network from inside
+const flags = '--cap-add=NET_ADMIN';
 
-const localSDK = {
-  'Test all commands(good-path)': fakeSDK({
-    // eslint-disable-next-line arrow-body-style
-    invoke: async () => {
-      return {
-        body: 'Hello World!',
-        statusCode: 200,
-      };
-    },
-  }),
-  'Test deploy(good-path)': fakeSDK(),
-};
+// The environment variables we want propagated to Docker
+const envVars = [
+  'BINARIS_INVOKE_ENDPOINT',
+  'BINARIS_DEPLOY_ENDPOINT',
+  'BINARIS_API_KEY'];
 
-const getSDK = function getSDK(testSDKMap, name) {
-  if (useMockSDK && Object.prototype.hasOwnProperty.call(testSDKMap, name)) {
-    return testSDKMap[name];
-  }
-  return clone(binarisSDK);
-};
-
+/**
+ * Imitates stdc sleep behavior using es6 async/await
+ *
+ * @param {int} ms - the duration in milliseconds to sleep
+ */
 const msleep = function msleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 };
 
+/**
+ * Create a Docker container for each test before it runs.
+ * This way all test runs are isolated thereby opening up
+ * many testing avenues.
+ */
+test.beforeEach(async (t) => {
+  // eslint-disable-next-line no-param-reassign
+  t.context.container = new Container(dockerImage,
+    dockerPassword, testMountDir);
+  await t.context.container.create();
+  await t.context.container.giveUserVolumeAccess(dockerUser, dockerGroup);
+});
+
+/**
+ * Always call `remove` on your container before exit
+ */
+test.afterEach.always(async (t) => {
+  if (t.context.container.isCreated()) {
+    await t.context.container.remove();
+  }
+});
+
+/**
+ * Iterates over the YAML CLI specification separating and testing each
+ * `test` entry separately.
+ */
 const planYAML = yaml.safeLoad(fs.readFileSync(process.env.BINARIS_TEST_SPEC_PATH || './test/CLISpec.yml', 'utf8'));
 planYAML.forEach((rawSubTest) => {
   test(rawSubTest.test, async (t) => {
-    // change to the specified work directory
-    if (rawSubTest.work_dir) {
-      shell.cd(rawSubTest.work_dir);
-    }
-
+    // Create our environment variable string from the env list
+    const envs = `-e ${envVars.join(' -e ')}`;
     if (rawSubTest.setup) {
       for (const setupStep of rawSubTest.setup) {
-        t.true(shell.exec(setupStep).code === 0);
+        // eslint-disable-next-line no-await-in-loop
+        await t.context.container.run(`${envs}${flags}`, setupStep);
       }
     }
 
-    const subTestSDK = getSDK(localSDK, rawSubTest.test);
-    const stepLogger = createLogger();
     for (const step of rawSubTest.steps) {
-      delete require.cache[require.resolve('commander')];
-      // this is done to ensure that the CLI doesn't reuse
-      // an instance of commander
       if (step.delay) {
         // eslint-disable-next-line no-await-in-loop
         await msleep(step.delay);
       }
-      // this creates node style process.argv inputs and calls the CLI
-      const CLI = createCLIStub(subTestSDK, stepLogger);
-      // eslint-disable-next-line no-await-in-loop
-      const exitCode = await CLI(['', ...stringArgv(step.in)]);
-      const logs = stepLogger.releaseOutputString();
-      t.is(exitCode, (step.exit || 0));
-      t.true(globToRegExp(step.out).test(logs));
+      try {
+        // eslint-disable-next-line no-await-in-loop
+        const output = await t.context.container.run(`${envs} ${flags}`, step.in, true);
+        t.true(globToRegExp(step.out).test(output.slice(0, -1)));
+      } catch (err) {
+        // Annoying byproduct of the child_process in node. Because we
+        // have no guarantees regarding ordering, we must test both possible
+        // orderings to ensure we have a match.
+        const orderOne = `${err.stdout}${err.stderr}`.slice(0, -1);
+        const orderTwo = `${err.stderr}${err.stdout}`.slice(0, -1);
+        if (globToRegExp(step.out).test(orderOne)) {
+          t.true(globToRegExp(step.out).test(orderOne));
+        } else {
+          t.true(globToRegExp(step.out).test(orderTwo));
+        }
+        t.is(err.code, (step.exit || 0));
+      }
     }
   });
 });
