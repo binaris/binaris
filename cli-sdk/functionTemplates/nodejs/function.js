448c4728c18125c74200bb74c46e2d7627a009ef
Ryland Goldstein <ryland@binaris.com>
2017-11-15 03:15:14 +0000

    changed function.js template to use console

--- cli-sdk/functionTemplates/nodejs/function.js
+++ cli-sdk/functionTemplates/nodejs/function.js
@@ -1,4 +1,5 @@
 exports.handler = (event, context, callback) => {
   const name = event.queryStringParameters.name || event.body.name || 'World';
+  console.log(`Hello ${name}!`);
   callback(null, { statusCode: 200, body: `Hello ${name}!` });
 };
