462c08e2bea143736c1df8b7d9cabafc5e99be07
Ryland Goldstein <ryland@binaris.com>
2018-03-04 15:56:07 -0800

    since command

--- sdk/logs.js
+++ sdk/logs.js
@@ -10,24 +10,24 @@ const { logEndpoint } = require('./config');
  * @param {string} apiKey - Binaris API key used to authenticate function invocation
  * @param {string} startingEntry - the log entry to start from(but not including)
  */
-const logs = async function logs(functionName, apiKey, startingEntry) {
+const logs = async function logs(functionName, apiKey, startingEntry, mode) {
   const options = {
     json: true,
     forever: true,
     resolveWithFullResponse: true,
   };
-
   // determine which mode to query the log endpoint with
-  const mode = startingEntry ? 'tailf' : 'logs';
   options.url = urljoin(`https://${logEndpoint}`, 'v1',
     'logs', `${apiKey}-${functionName}`, mode);
-  if (mode === 'tailf') {
-    options.qs = {
-      startEpochMillisecs: startingEntry.timestamp,
-      token: startingEntry.offset,
-    };
+  if (startingEntry) {
+    options.qs = {};
+    if (startingEntry.timestamp) {
+      options.qs.startEpochMillisecs = startingEntry.timestamp;
+    }
+    if (startingEntry.offset) {
+      options.qs.token = startingEntry.offset;
+    }
   }
-
   return (await rp.get(options)).body || [];
 };
 
