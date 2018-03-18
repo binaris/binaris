b2d8616dcc927ee5d273a7c505f24483cc502b3b
Roey Berman <roey.berman@gmail.com>
2018-03-18 17:39:45 +0200

    Modify logs code to use new API
    Even newer logs API :-0
    
    more internal loggin
    
    bolt's bunyan captures user output into `msg`, not `message`.

--- sdk/logs.js
+++ sdk/logs.js
@@ -1,5 +1,6 @@
 const urljoin = require('urljoin');
 const rp = require('request-promise-native');
+const logger = require('../lib/logger');
 
 const { logEndpoint } = require('./config');
 
@@ -8,27 +9,30 @@ const { logEndpoint } = require('./config');
  *
  * @param {string} functionName - name of functions whose logs will be retrieved
  * @param {string} apiKey - Binaris API key used to authenticate function invocation
- * @param {string} startingEntry - the log entry to start from(but not including)
+ * @param {boolean} follow - As in tail -f
+ * @param {Date} startAfter - Datetime of first log record to fetch
+ * @param {string} token - Token for fetching next page (returned by this function)
  */
-const logs = async function logs(functionName, apiKey, startingEntry, mode) {
+const logs = async function logs(functionName, apiKey, follow, startAfter, token) {
   const options = {
     json: true,
     forever: true,
     resolveWithFullResponse: true,
+    url: urljoin(`https://${logEndpoint}`, 'v1', 'logs', `${apiKey}-${functionName}`),
+    qs: {
+      startAfter,
+      token,
+      follow,
+    },
   };
-  // determine which mode to query the log endpoint with
-  options.url = urljoin(`https://${logEndpoint}`, 'v1',
-    'logs', `${apiKey}-${functionName}`, mode);
-  if (startingEntry) {
-    options.qs = {};
-    if (startingEntry.timestamp) {
-      options.qs.startEpochMillisecs = startingEntry.timestamp;
-    }
-    if (startingEntry.offset) {
-      options.qs.token = startingEntry.offset;
-    }
-  }
-  return (await rp.get(options)).body || [];
+
+  logger.debug('Logs request', options);
+  const { statusCode, headers, body } = await rp.get(options);
+  logger.debug('Logs response', { statusCode, headers, body });
+  return {
+    body,
+    nextToken: headers['x-binaris-next-token'],
+  };
 };
 
 module.exports = logs;
