a7d6ff27251beab48befeba36d5fa36ee2993fce
Roey Berman <roey.berman@gmail.com>
2018-03-28 16:57:10 +0300

    Add retry+backoff to logs request

--- sdk/logs.js
+++ sdk/logs.js
@@ -4,6 +4,8 @@ const logger = require('../lib/logger');
 
 const { logEndpoint } = require('./config');
 
+const msleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
 /**
  * Retrieves the logs of a previously deployed Binaris function.
  *
@@ -13,7 +15,7 @@ const { logEndpoint } = require('./config');
  * @param {Date} startAfter - Datetime of first log record to fetch
  * @param {string} token - Token for fetching next page (returned by this function)
  */
-const logs = async function logs(functionName, apiKey, follow, startAfter, token) {
+async function logs(functionName, apiKey, follow, startAfter, token) { // eslint-disable-line consistent-return,max-len
   const options = {
     json: true,
     forever: true,
@@ -26,13 +28,24 @@ const logs = async function logs(functionName, apiKey, follow, startAfter, token
     },
   };
 
-  logger.debug('Logs request', options);
-  const { statusCode, headers, body } = await rp.get(options);
-  logger.debug('Logs response', { statusCode, headers, body });
-  return {
-    body,
-    nextToken: headers['x-binaris-next-token'],
-  };
-};
+  for (let attempt = 1, backoff = 3; attempt <= 3; attempt += 1, backoff *= 2) {
+    try {
+      // eslint-disable-next-line no-await-in-loop
+      const { statusCode, headers, body } = await rp.get(options);
+      logger.debug('Logs response', { statusCode, headers, body });
+      return {
+        body,
+        nextToken: headers['x-binaris-next-token'],
+      };
+    } catch (err) {
+      if (!err.statusCode || err.statusCode < 500 || err.statusCode >= 600) { // Not a 5xx error
+        throw err;
+      }
+      logger.debug('Failed to fetch logs', { statusCode: err.statusCode, message: err.message, attempt });
+      // eslint-disable-next-line no-await-in-loop
+      await msleep(backoff);
+    }
+  }
+}
 
 module.exports = logs;
