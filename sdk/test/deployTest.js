5e81bb1fe2f2228a405543a42ed5acaeb32c59a0
Roey Berman <roey.berman@gmail.com>
2018-04-29 13:37:44 +0300

    Fix SDK tests

--- sdk/test/deployTest.js
+++ sdk/test/deployTest.js
@@ -1,10 +1,10 @@
 // SDK dir structure confuses eslint
 /* eslint-disable import/no-extraneous-dependencies */
 const { test } = require('ava');
-const nock = require('nock');
 const fs = require('mz/fs');
 const fse = require('fs-extra');
 const { generate } = require('randomstring');
+const nock = require('nock');
 
 // standard length of Binaris API key
 const APIKeyLength = 20;
@@ -30,53 +30,61 @@ test.afterEach.always(async (t) => {
   nock.cleanAll();
 });
 
-const deployEndpoint = 'https://api.binaris.com';
+test.serial('Just test deploy (good-path)', async (t) => {
+  const deployEndpoint = generate(10);
 
-test('Just test deploy (good-path)', async (t) => {
-  delete process.env.BINARIS_DEPLOY_ENDPOINT;
   // eslint-disable-next-line global-require
   const deploy = require('../deploy');
   const digestObj = { digest: 'fakedigest' };
   // eslint-disable-next-line no-unused-vars
-  const deployMock = nock(deployEndpoint)
+  const deployMock = nock(`https://${deployEndpoint}`);
+  deployMock
     .post('/v2/code')
     .matchHeader('X-Binaris-Api-Key', testApiKey)
     .matchHeader('Content-Type', 'application/gzip')
-    .reply(200, digestObj)
-    .post(`/v2/conf/${testApiKey}/${testFuncName}`, testFuncConf)
-    .reply(200, digestObj)
-    .post(`/v2/tag/${testApiKey}/${testFuncName}/latest`, digestObj)
     .reply(200, digestObj);
+
+  deployMock
+    .post(`/v2/conf/${testApiKey}/${testFuncName}`, testFuncConf)
+    .reply(200, digestObj);
+
+  deployMock
+    .post(`/v2/tag/${testApiKey}/${testFuncName}/latest`, digestObj)
+    .reply(200, { status: 'ok' });
+
   const response = await deploy(testFuncName, testApiKey,
-    testFuncConf, t.context.fakeTarFileName);
+    testFuncConf, t.context.fakeTarFileName, deployEndpoint);
   t.is(200, response.status);
-  t.is(digestObj.digest, response.body.digest);
+  t.deepEqual({ status: 'ok' }, response.body);
 });
 
-// test('Test deploy with bad key (bad-path)', async (t) => {
-//   delete process.env.BINARIS_DEPLOY_ENDPOINT;
-//   // eslint-disable-next-line global-require
-//   const deploy = require('../deploy');
+test.serial('Test deploy with bad key (bad-path)', async (t) => {
+  // eslint-disable-next-line global-require
+  const deploy = require('../deploy');
 
-//   const someBadKey = generate(APIKeyLength);
-//   // eslint-disable-next-line no-unused-vars
-//   const deployMock = nock('https://api.binaris.com')
-//     .post(`/v1/function/${someBadKey}-${testFuncName}`)
-//     .query(testFuncConf)
-//     .reply(403, { errorCode: 'ERR_BAD_KEY' });
-//   const response = await deploy(testFuncName, someBadKey,
-//     testFuncConf, t.context.fakeTarFileName);
-//   t.is(403, response.status);
-//   t.is('ERR_BAD_KEY', response.body.errorCode);
-// });
+  const deployEndpoint = generate(10);
 
-test('Test deploy with no backend (bad-path)', async (t) => {
-  process.env.BINARIS_DEPLOY_ENDPOINT = 'invalidbinaris.endpoint';
+  const someBadKey = generate(APIKeyLength);
+  // eslint-disable-next-line no-unused-vars
+  const deployMock = nock(`https://${deployEndpoint}`)
+    .post('/v2/code')
+    .matchHeader('X-Binaris-Api-Key', someBadKey)
+    .matchHeader('Content-Type', 'application/gzip')
+    .reply(403, { errorCode: 'ERR_BAD_KEY' });
+
+  const response = await deploy(testFuncName, someBadKey,
+    testFuncConf, t.context.fakeTarFileName, deployEndpoint);
+
+  t.is(403, response.status);
+  t.is('ERR_BAD_KEY', response.body.errorCode);
+});
+
+test.serial('Test deploy with no backend (bad-path)', async (t) => {
   // eslint-disable-next-line global-require
   const deploy = require('../deploy');
 
   const response = await deploy(testFuncName, testApiKey,
-    testFuncConf, t.context.fakeTarFileName);
+    testFuncConf, t.context.fakeTarFileName, 'invalidbinaris.endpoint');
   t.true(response.error !== undefined);
 });
 
