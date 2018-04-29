484bda4d3aab581ea2ce8d854a0003c2c2bc3d3e
Roey Berman <roey.berman@gmail.com>
2018-04-29 13:37:44 +0300

    Fix SDK tests

--- sdk/deploy.js
+++ sdk/deploy.js
@@ -5,6 +5,14 @@ const rp = require('request-promise-native');
 
 const { getDeployEndpoint } = require('./config');
 
+
+class HTTPError extends Error {
+  constructor(response) {
+    super(response.statusMessage);
+    this.response = response;
+  }
+}
+
 /**
  * Deploys a tarball, whose contents represent a Binaris function deployment
  *
@@ -33,7 +41,7 @@ const deployCode = async function deployCode(deployURLBase, apiKey, tarPath) {
         if (uploadErr) {
           reject(uploadErr);
         } else if (uploadResponse.statusCode !== 200) {
-          reject(new Error(uploadResponse.statusMessage));
+          reject(new HTTPError(uploadResponse));
         } else {
           resolve(uploadResponse);
         }
@@ -51,7 +59,7 @@ const deployCode = async function deployCode(deployURLBase, apiKey, tarPath) {
  * @param {string} funcName - name of function whose conf is being deployed
  * @param {string} funcConf - configuration object to deploy for the function
  *
- * @return {object} - response of conf deployment
+ * @return {object} - response of tag operation
  */
 const deployConf = async function deployConf(deployURLBase, apiKey, funcName, funcConf) {
   const confDeployOptions = {
@@ -60,13 +68,15 @@ const deployConf = async function deployConf(deployURLBase, apiKey, funcName, fu
     json: true,
   };
   const { digest } = await rp.post(confDeployOptions);
+  // const digest = 'fakedigest';
   const tagDeployOptions = {
     url: urljoin(deployURLBase, 'v2', 'tag', apiKey, funcName, 'latest'),
     json: true,
     body: { digest },
     resolveWithFullResponse: true,
   };
-  return rp.post(tagDeployOptions);
+  const response = await rp.post(tagDeployOptions);
+  return response;
 };
 
 /**
@@ -76,18 +86,28 @@ const deployConf = async function deployConf(deployURLBase, apiKey, funcName, fu
  * @param {string} apiKey - Binaris API key used to authenticate function removal
  * @param {object} funcConf - configuration of the function to deploy
  * @param {string} tarPath - path of the tarball containing the function to deploy
+ * @param {string} endpoint - binaris deploy endpoint
  *
- * @returns {string} - curlable URL of the endpoint used to invoke your function
+ * @returns {object} - response { status, body }
  */
-const deploy = async function deploy(funcName, apiKey, funcConf, tarPath) {
+const deploy = async function deploy(
+  funcName,
+  apiKey,
+  funcConf,
+  tarPath,
+  endpoint = getDeployEndpoint(),
+) {
   try {
-    const deployURLBase = `https://${getDeployEndpoint()}`;
+    const deployURLBase = `https://${endpoint}`;
     const codeDigest = await deployCode(deployURLBase, apiKey, tarPath);
     const confWithDigest = Object.assign({}, funcConf, { codeDigest });
     const confDeployResp = await deployConf(deployURLBase, apiKey,
       funcName, confWithDigest);
     return { status: confDeployResp.statusCode, body: confDeployResp.body };
   } catch (err) {
+    if (err instanceof HTTPError) {
+      return { status: err.response.statusCode, body: err.response.body };
+    }
     // NOTE: This 'err' returned in the error field is in NodeJS error format
     return { error: err };
   }
