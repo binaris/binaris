8b250f682d3e83f4878063646fe56da960bfdb56
Roey Berman <roey.berman@gmail.com>
2018-05-06 11:06:43 +0300

    Revert "Revert feature-tags-complete"
    This reverts commit 259254bab3ffdfe0041e5291829e81da76792f33.

--- sdk/test/deployTest.js
+++ sdk/test/deployTest.js
@@ -1,10 +1,10 @@
 // SDK dir structure confuses eslint
 /* eslint-disable import/no-extraneous-dependencies */
 const { test } = require('ava');
-const nock = require('nock');
 const fs = require('mz/fs');
 const fse = require('fs-extra');
 const { generate } = require('randomstring');
+const nock = require('nock');
 
 // standard length of Binaris API key
 const APIKeyLength = 20;
@@ -13,6 +13,8 @@ const testApiKey = generate(APIKeyLength);
 const testFuncConf = {
   file: 'function.js',
   entrypoint: 'handler',
+  runtime: 'node8',
+  codeDigest: 'fakedigest',
 };
 
 const testFuncName = 'binarisTestDeployFunction';
@@ -28,46 +30,61 @@ test.afterEach.always(async (t) => {
   nock.cleanAll();
 });
 
-test('Just test deploy (good-path)', async (t) => {
-  delete process.env.BINARIS_DEPLOY_ENDPOINT;
+test.serial('Just test deploy (good-path)', async (t) => {
+  const deployEndpoint = generate(10);
+
   // eslint-disable-next-line global-require
   const deploy = require('../deploy');
-
+  const digestObj = { digest: 'fakedigest' };
   // eslint-disable-next-line no-unused-vars
-  const deployMock = nock('https://api.binaris.com')
-    .post(`/v1/function/${testApiKey}-${testFuncName}`)
-    .query(testFuncConf)
-    .reply(200, { status: 'OK' });
+  const deployMock = nock(`https://${deployEndpoint}`);
+  deployMock
+    .post('/v2/code')
+    .matchHeader('X-Binaris-Api-Key', testApiKey)
+    .matchHeader('Content-Type', 'application/gzip')
+    .reply(200, digestObj);
+
+  deployMock
+    .post(`/v2/conf/${testApiKey}/${testFuncName}`, testFuncConf)
+    .reply(200, digestObj);
+
+  deployMock
+    .post(`/v2/tag/${testApiKey}/${testFuncName}/latest`, digestObj)
+    .reply(200, { status: 'ok' });
+
   const response = await deploy(testFuncName, testApiKey,
-    testFuncConf, t.context.fakeTarFileName);
+    testFuncConf, t.context.fakeTarFileName, deployEndpoint);
   t.is(200, response.status);
-  t.is('OK', response.body.status);
+  t.deepEqual({ status: 'ok' }, response.body);
 });
 
-test('Test deploy with bad key (bad-path)', async (t) => {
-  delete process.env.BINARIS_DEPLOY_ENDPOINT;
+test.serial('Test deploy with bad key (bad-path)', async (t) => {
   // eslint-disable-next-line global-require
   const deploy = require('../deploy');
+
+  const deployEndpoint = generate(10);
 
   const someBadKey = generate(APIKeyLength);
   // eslint-disable-next-line no-unused-vars
-  const deployMock = nock('https://api.binaris.com')
-    .post(`/v1/function/${someBadKey}-${testFuncName}`)
-    .query(testFuncConf)
+  const deployMock = nock(`https://${deployEndpoint}`)
+    .post('/v2/code')
+    .matchHeader('X-Binaris-Api-Key', someBadKey)
+    .matchHeader('Content-Type', 'application/gzip')
     .reply(403, { errorCode: 'ERR_BAD_KEY' });
+
   const response = await deploy(testFuncName, someBadKey,
-    testFuncConf, t.context.fakeTarFileName);
+    testFuncConf, t.context.fakeTarFileName, deployEndpoint);
+
   t.is(403, response.status);
   t.is('ERR_BAD_KEY', response.body.errorCode);
 });
 
-test('Test deploy with no backend (bad-path)', async (t) => {
-  process.env.BINARIS_DEPLOY_ENDPOINT = 'invalidbinaris.endpoint';
+test.serial('Test deploy with no backend (bad-path)', async (t) => {
   // eslint-disable-next-line global-require
   const deploy = require('../deploy');
 
   const response = await deploy(testFuncName, testApiKey,
-    testFuncConf, t.context.fakeTarFileName);
+    testFuncConf, t.context.fakeTarFileName, 'invalidbinaris.endpoint');
   t.true(response.error !== undefined);
 });
 
