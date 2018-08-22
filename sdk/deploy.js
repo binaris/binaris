ce5114d797a286f36c1e690771ca274bf3dde8f7
Ryland Goldstein <ryland@binaris.com>
2018-08-22 13:21:32 -0700

    unify error responses in deploy/remove

--- sdk/deploy.js
+++ sdk/deploy.js
@@ -29,18 +29,22 @@ const deployCode = async function deployCode(deployURLBase, apiKey, tarPath) {
   // request-promise module explicitly discourages using
   // request-promise for pipe
   // https://github.com/request/request-promise
-  const codeDeployment = await (new Promise((resolve, reject) => {
-    fs.createReadStream(tarPath)
-      .pipe(request.post(codeDeployOptions, (uploadErr, uploadResponse) => {
-        if (uploadErr) {
-          reject(uploadErr);
-        } else {
-          resolve(uploadResponse);
-        }
-      }));
-  }));
-  validateResponse(codeDeployment);
-  return codeDeployment.body.digest;
+  try {
+    const codeDeployment = await (new Promise((resolve, reject) => {
+      fs.createReadStream(tarPath)
+        .pipe(request.post(codeDeployOptions, (uploadErr, uploadResponse) => {
+          if (uploadErr) {
+            reject(uploadErr);
+          } else {
+            resolve(uploadResponse);
+          }
+        }));
+    }));
+    validateResponse(codeDeployment);
+    return codeDeployment.body.digest;
+  } catch (err) {
+    throw new Error(`Error: ${err.message}`);
+  }
 };
 
 /**
