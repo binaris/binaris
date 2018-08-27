8d3f371a2f8d1f579250659f38b92876b863600a
Ryland Goldstein <ryland@binaris.com>
2018-08-27 11:00:24 -0700

    send version with every request

--- sdk/handleError.js
+++ sdk/handleError.js
@@ -6,17 +6,19 @@ const logger = require('../lib/logger');
 
 const { translateErrorCode } = require('binaris-pickle');
 
+const { version } = require('../package.json');
+
 class APIError extends Error {}
 
 function validateResponse(response) {
+  const errorText = get(response, 'body.error');
+  if (errorText) {
+    throw new APIError(errorText);
+  }
+
   const errorCode = get(response, 'body.errorCode');
   if (errorCode) {
     throw new APIError(`Error: ${translateErrorCode(errorCode)}`);
-  }
-
-  const errorText = get(response, 'body.errorText');
-  if (errorText) {
-    throw new APIError(errorText);
   }
 
   if (response.statusCode < 200 || response.statusCode >= 300) {
@@ -32,6 +34,10 @@ async function loggedRequest(options, type = 'post') {
     simple: false,
     resolveWithFullResponse: true,
     ...options,
+    headers: {
+      ...(options.headers || {}),
+      'X-binaris-CLI-Version': version
+    },
   });
   logger.debug('raw response', inspect(response, { depth: null }));
   return response;
@@ -44,7 +50,8 @@ async function getValidatedBody(options, type = 'post') {
 
 module.exports = {
   APIError,
+  getValidatedBody,
   loggedRequest,
   validateResponse,
-  getValidatedBody,
+  version,
 };
