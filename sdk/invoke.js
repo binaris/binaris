fbc986f1a1f668d82c3f0535be36acaaae48e428
Ryland Goldstein <ryland@binaris.com>
2018-10-16 11:26:42 -0700

    add spec tests for new error system

--- sdk/invoke.js
+++ sdk/invoke.js
@@ -31,14 +31,14 @@ const invoke = async function invoke(funcName, apiKey, funcData) {
     try {
       parsedError = JSON.parse(err.error);
     } catch (nestedErr) {
-      throw new Error(`Failed to parse error ${nestedErr}`);
+      throw new Error(err.error || err);
     }
-    if (parsedError.stack && parsedError.request_id) {
-      throw new Error(`${parsedError.error}\n${parsedError.stack}`);
+    if (parsedError.stack && (parsedError.error || parsedError.message)) {
+      throw new Error(`${parsedError.error || parsedError.message}\n${parsedError.stack}`);
     } else if (parsedError.message && parsedError.request_id) {
       throw new Error(`${parsedError.message}\nrequest_id: ${parsedError.request_id}`);
     }
-    throw new Error(parsedError);
+    throw new Error(err.error || err);
   }
 };
 
