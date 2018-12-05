42c1475bf4e12c23335b61e0e672f0c84caf8af4
Roey Berman <roey.berman@gmail.com>
2018-12-05 09:09:08 +0200

    Support V2 URL for remove

--- sdk/remove.js
+++ sdk/remove.js
@@ -1,20 +1,21 @@
 'use strict';
 
-const urljoin = require('urljoin');
-
-const { getDeployEndpoint } = require('./config');
+const { getConfTagUrl } = require('./url');
 const { getValidatedBody } = require('./handleError');
 
 /**
  * Removes the function from the Binaris cloud.
  *
+ * @param {string} accountId - Binaris account ID
  * @param {string} funcName - the name of the function to remove
  * @param {string} apiKey - the apiKey to authenticate the removal with
  */
-const remove = async function remove(funcName, apiKey) {
+const remove = async function remove(accountId, funcName, apiKey) {
   const options = {
-    url: urljoin(`https://${getDeployEndpoint()}`, 'v2', 'tag', apiKey, funcName, 'latest'),
-    json: true,
+    url: getConfTagUrl(accountId, funcName, apiKey, 'latest'),
+    headers: {
+      'X-Binaris-Api-Key': apiKey,
+    },
   };
   return getValidatedBody(options, 'delete');
 };
