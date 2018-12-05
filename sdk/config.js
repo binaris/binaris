1a6cc0aef9edf0359cf55c43e3a14071a05950bf
arikmaor <arik@binaris.com>
2018-12-05 11:12:27 +0200

    Support for realm prefix in apikey

--- sdk/config.js
+++ sdk/config.js
@@ -12,9 +12,16 @@ const getLogEndpoint = function getLogEndpoint() {
   return process.env.BINARIS_LOG_ENDPOINT || 'log.binaris.com';
 };
 
+const forceRealm = function forceRealm(realm) {
+  process.env.BINARIS_DEPLOY_ENDPOINT = process.env.BINARIS_DEPLOY_ENDPOINT || `api-${realm}.binaris.com`;
+  process.env.BINARIS_INVOKE_ENDPOINT = process.env.BINARIS_INVOKE_ENDPOINT || `run-${realm}.binaris.com`;
+  process.env.BINARIS_LOG_ENDPOINT = process.env.BINARIS_LOG_ENDPOINT || `log-${realm}.binaris.com`;
+};
+
 module.exports = {
   getDeployEndpoint,
   getInvokeEndpoint,
   getLogEndpoint,
+  forceRealm,
 };
 
