ac38bc86cbdac0a916ac25c6db35c0ee1fb2f70a
Roey Berman <roey.berman@gmail.com>
2018-12-05 12:42:00 +0200

    Support new stats URL

--- sdk/stats.js
+++ sdk/stats.js
@@ -1,22 +1,23 @@
 'use strict';
 
-const urljoin = require('urljoin');
 const logger = require('../lib/logger');
 const { getValidatedBody } = require('./handleError');
-
-const { getDeployEndpoint } = require('./config');
+const { getStatsUrl } = require('./url');
 
 /**
  * Retrieves account usage statistics
  *
+ * @param {string} accountId - Binaris account ID
  * @param {string} apiKey - Binaris API key
  * @param {Date} since - All stats since the date-time
  * @param {Date} until - All stats until the date-time
  */
-const stats = async function stats(apiKey, since, until) { // eslint-disable-line consistent-return,max-len
-  const statsURLBase = `https://${getDeployEndpoint()}`;
+const stats = async function stats(accountId, apiKey, since, until) { // eslint-disable-line consistent-return,max-len
   const statsOptions = {
-    url: urljoin(statsURLBase, 'v2', 'metrics', apiKey),
+    url: getStatsUrl(accountId, apiKey),
+    headers: {
+      'X-Binaris-Api-Key': apiKey,
+    },
     body: {
       since,
       until,
