4f4afb725740bad3d9748fad7795ab19bdc0c628
Jake Langholz <jake@binaris.com>
2019-01-02 12:03:09 -0800

    Removed condition, added realm to feedback message. Fixed incorrect description.

--- sdk/feedback.js
+++ sdk/feedback.js
@@ -4,14 +4,10 @@ const { IncomingWebhook } = require('@slack/client');
 
 const feedback = async function feedback(realm, funcData) {
   let slackWebhookUrl;
-  if (realm === 'prod') {
-    slackWebhookUrl = String('https://hooks.slack.com/services/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');
-  } else {
-    slackWebhookUrl = String('https://hooks.slack.com/services/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');
-  }
+  slackWebhookUrl = String('https://hooks.slack.com/services/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');
 
   const webhook = new IncomingWebhook(slackWebhookUrl);
-  return webhook.send(`User with email ${funcData.email} provided feedback: ${funcData.message}`);
+  return webhook.send(`User with email ${funcData.email} on realm ${realm} provided feedback: ${funcData.message}`);
 };
 
 module.exports = feedback;
