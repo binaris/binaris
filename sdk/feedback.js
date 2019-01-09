0be3ab7a9bff9eabdfcadac3752c790183e62424
Jake Langholz <jake@binaris.com>
2019-01-09 12:19:33 -0800

    Fixed linting errors.

--- sdk/feedback.js
+++ sdk/feedback.js
@@ -3,13 +3,14 @@
 const { IncomingWebhook } = require('@slack/client');
 
 const feedback = async function feedback(realm, funcData) {
-  let slackWebhookUrl;
-  slackWebhookUrl = String('https://hooks.slack.com/services/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');
+  const slackWebhookUrl = String('https://hooks.slack.com/services/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');
+  let result = {};
 
   if (!funcData.message.includes('ZL3FtCMyqeRQjP5GESTRbDRwMEjshHhjYAXHiMRZg3EOtutyWvEWcYJN7704ch3W')) {
-  const webhook = new IncomingWebhook(slackWebhookUrl);
-  return webhook.send(`User with email ${funcData.email} on realm ${realm} provided feedback: ${funcData.message}`);
- }
+    const webhook = new IncomingWebhook(slackWebhookUrl);
+    result = webhook.send(`User with email ${funcData.email} on realm ${realm} provided feedback: ${funcData.message}`);
+  }
+  return result;
 };
 
 module.exports = feedback;
