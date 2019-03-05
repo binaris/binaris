125ebe3eca9bb946ba3008a37ddd4b915038f1ba
Michael Adda <michael@binaris.com>
2019-03-05 14:36:10 +0200

    Update slack web hook url

--- sdk/feedback.js
+++ sdk/feedback.js
@@ -3,7 +3,7 @@
 const { IncomingWebhook } = require('@slack/client');
 
 const feedback = async function feedback(realm, funcData) {
-  const slackWebhookUrl = String('https://hooks.slack.com/services/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');
+  const slackWebhookUrl = String('https://hooks.slack.com/services/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');
   let result = {};
 
   if (!funcData.message.includes('ZL3FtCMyqeRQjP5GESTRbDRwMEjshHhjYAXHiMRZg3EOtutyWvEWcYJN7704ch3W')) {
